5b82b62d34d78f174b54e0b14dbd8740712bf2fb
Alex Velazquez <avelasquez075@gmail.com>
2021-05-03 13:57:47 -0700

    pass desctructred props of tour object, article with image

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+// destructuring the properties of the tour object being passed in
+const Tour = ({id, image, info, price, name}) => {
+  return (
+    <article className='single-tour'>
+      <img src={image} alt={name} />
+    </article>
+  )
 };
 
 export default Tour;
