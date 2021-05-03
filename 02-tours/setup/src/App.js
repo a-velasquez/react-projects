27b9194362baf8420efa9e4c3239dc61c6b7dd54
Alex Velazquez <avelasquez075@gmail.com>
2021-05-03 13:15:20 -0700

    move await and error to try and catch block

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -6,14 +6,21 @@ import Tours from './Tours'
 const url = 'https://course-api.com/react-tours-project'
 
 function App() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
   const fetchTours = async () => {
     setLoading(true);
-    const response = await fetch(url);
-    const tours = await response.json();
-    console.log(tours)
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
   }
 
   useEffect(() => {
