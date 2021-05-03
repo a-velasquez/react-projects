3b3c69ec2081a1881d8594f60ec350b071d50909
Alex Velazquez <avelasquez075@gmail.com>
2021-05-03 15:42:32 -0700

    add Review component

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import Review from './Review';
+import React from "react";
+import Review from "./Review";
+
 function App() {
   return (
     <main>
-      <section className='container'>
-        <div className='title'>
-          <h2>
-            our reviews
-          </h2>
-          <div className='underline'></div>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
         </div>
+        <Review />
       </section>
     </main>
-  )
+  );
 }
 
 export default App;
