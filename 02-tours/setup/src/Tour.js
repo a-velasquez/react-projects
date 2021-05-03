341c5f976b579f2215c9ffadd1ae97f1ec1ebbcf
Alex Velazquez <avelasquez075@gmail.com>
2021-05-03 14:14:27 -0700

    add state for readMore button, add readMore, change title of button dynamically based on state

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 
 // destructuring the properties of the tour object being passed in
-const Tour = ({id, image, info, price, name}) => {
+const Tour = ({ id, image, info, price, name }) => {
+  
+  const [readMore, setReadMore] = useState(false)
+
+
   return (
     <article className='single-tour'>
       <img src={image} alt={name} />
@@ -11,8 +15,11 @@ const Tour = ({id, image, info, price, name}) => {
           <h4 className='tour-price'>${price}</h4>
         </div>
         <p>
-          {info}
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          {/* since we are toggling pass setReadMore !readMore, essentialy the opposite of the current state of readMore */}
+          <button onClick={() => setReadMore(!readMore)}>{readMore ? 'show less' : 'read more'}</button>
         </p>
+        <button className='delete-btn'>not interested</button>
       </footer>
     </article>
   )
