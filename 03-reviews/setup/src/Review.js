88639486bf7263d35dd885ebaff91ec1df5f17b8
Alex Velazquez <avelasquez075@gmail.com>
2021-05-03 17:33:07 -0700

    add span with quote icon, render author, job, info

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -10,8 +10,14 @@ const Review = () => {
   return (
     <article className='review'>
       <div className='img-container'>
-        <img src={image} alt={name} className='person-img'/>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
       </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
     </article>
   );
 };
