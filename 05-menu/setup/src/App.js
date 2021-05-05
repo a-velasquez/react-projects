9ae7241af3c9b0c28cc7211e990a63cbf4f64b38
Alex Velazquez <avelasquez075@gmail.com>
2021-05-04 23:48:25 -0700

    add state values for menuItems and categories

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -4,6 +4,11 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
+
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState([])
+  console.log(items)
+
   return <h2>menu project setup</h2>;
 }
 
