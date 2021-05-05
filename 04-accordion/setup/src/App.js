c90611cb2989bf390bb4fc01e168a8ce1f4f2b5a
Alex Velazquez <avelasquez075@gmail.com>
2021-05-04 22:15:33 -0700

    set state values for questions passing in data from data.js

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+
 function App() {
+
+  const [questions, setQuestions] = useState(data)
+
   return <h2>accordion project setup</h2>;
 }
 
