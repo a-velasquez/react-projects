424d014feefdbe1ee80665e4ad43eba4eb6fe77e
Alex Velazquez <avelasquez075@gmail.com>
2021-05-06 00:12:58 -0700

    cleanup code

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,35 +3,35 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+// const allCategories = new Set(items.map((item) => item.category))
+// only returns the categories present in the data, but we also need an 'all' btn
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
-
-  const [menuItems, setMenuItems] = useState(items)
-  const [categories, setCategories] = useState([])
-  // const allCategories = new Set(items.map((item) => item.category))
-  // only returns the categories present in the data, but we also need an 'all' btn
-  
-
-  const allCategories = ['all', ...new Set(items.map((item) => item.category))]
-  console.log(allCategories)
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) => {
-    const newItems = items.filter((item) => item.category === category)
-    setMenuItems(newItems)
-  }
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
-      <section className='menu section'>
-        <div className='title'>
+      <section className="menu section">
+        <div className="title">
           <h2>our menu</h2>
-          <div className='underline'>
-          </div>
+          <div className="underline"></div>
         </div>
-        <Categories filterItems={filterItems}/>
-        <Menu items={menuItems}/>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
       </section>
     </main>
-  )
+  );
 }
 
 export default App;
