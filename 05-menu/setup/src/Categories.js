dda44c31ae5de17fd3949d11585b3f11a93a5216
Alex Velazquez <avelasquez075@gmail.com>
2021-05-06 00:13:33 -0700

    add dynamicaly rendered buttons for all categories

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const Categories = ({filterItems}) => {
+const Categories = ({ categories, filterItems }) => {
   return (
-    <div className='btn-container'>
-      <button className='filter-btn' onClick={() => filterItems()}></button>
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
     </div>
-  )
+  );
 };
 
 export default Categories;
