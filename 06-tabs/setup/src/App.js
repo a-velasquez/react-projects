ff6e07c60ee301c74055bd51aa8ede9997902cb4
Alex Velazquez <avelasquez075@gmail.com>
2021-05-06 15:04:00 -0700

    iterate over duties, add icons

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -45,6 +45,16 @@ function App() {
         <article className='job-info'>
           <h3>{title}</h3>
           <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon'>
+                </FaAngleDoubleRight>
+              <p>{duty}</p>
+              </div>
+            )
+          })}
         </article>
       </div>
     </section>
