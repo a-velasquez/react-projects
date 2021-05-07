89d2af143ed1c364dd2c19a7536b88d109a82fe3
Alex Velazquez <avelasquez075@gmail.com>
2021-05-06 20:09:42 -0700

    add destrucutred props for person, map people array

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -16,7 +16,22 @@ function App() {
         </h2>
       </div>
       <div className='section-center'>
-         
+        {
+          people.map((person) => {
+            const { id, image, name, title, quote } = person
+            
+            return (
+              <article key={id}>
+                <img src={image} alt={name} className='person-img'/>
+                <h4>{name}</h4>
+                <p className='title'>{title}</p>
+                <p className='text'>{quote}</p>
+                <FaQuoteRight className='icon' />
+
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
