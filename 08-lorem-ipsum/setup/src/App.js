a1c9f6b643534c02e2ee4b8616e8664c220ad15a
Alex Velazquez <avelasquez075@gmail.com>
2021-05-10 23:32:33 -0700

    add state values and handleChange function for form

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+  const handleChange = (e) => {
+    setCount(e.target.value)
+    console.log(e.target.value)
+  }
   
   return (
   <h2>lorem ipsum project setup</h2>
