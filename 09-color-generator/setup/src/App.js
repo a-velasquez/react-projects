7f8e1769f338ecc34f350511d337e2c49c0be599
Alex Velazquez <avelasquez075@gmail.com>
2021-05-11 20:36:21 -0700

    set local state for list

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -5,7 +5,8 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('')
   const [error, setError] = useState(false)
-  const [list, setList] = useState(new Values('#f15025').all(10))
+  const [list, setList] = useState([])
+
   return <h2>color generator project</h2>
 }
 
