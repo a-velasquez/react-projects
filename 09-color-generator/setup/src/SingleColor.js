7ab443b1c041f64596590f6f92fd5a0c2e2311e6
Alex Velazquez <avelasquez075@gmail.com>
2021-05-13 16:50:39 -0700

    add const hex equal to hexColor prop, render hex value

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -4,17 +4,22 @@ import rgbToHex from './utils'
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false)
   const bgc = rgb.join(',')
+  const hex = `#${hexColor}`
+  console.log(hex)
+  console.log(bgc)
   // const bgc = rgb.toString())
   // console.log(bgc)
 
   return (
     <article
       className={`color ${index > 10 && 'color-light'}`}
+      // in-line style using javascript {} using template string to call css rgb function rgb() and pass rgb values through const bgc as arg using interpolation
       style={{ backgroundColor: `rgb(${bgc})` }}
     >
       <p className='percent-value'>
         {weight}
       </p>
+      <p className='color-value'>{hex}</p>
     </article>
   )
 }
