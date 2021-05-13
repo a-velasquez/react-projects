1ad334ef3d1aba0ec35dba7260af44bdc2e7b3b0
Alex Velazquez <avelasquez075@gmail.com>
2021-05-13 00:01:06 -0700

    add state value for alert and convert rgb array to string using .join(,) for css

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false)
+  const bgc = rgb.join(',')
+  // const bgc = rgb.toString())
+  // console.log(bgc)
+
+  return (
+    <article>
+      
+    </article>
+  )
 }
 
 export default SingleColor
