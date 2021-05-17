63b0b0f838f50dbfc46dde38ba7d8123aed4a0e6
Alex Velazquez <avelasquez075@gmail.com>
2021-05-16 23:34:55 -0700

    add  onClick to article to copy hex value to clipboard

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -12,11 +12,15 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
       className={`color ${index > 10 && 'color-light'}`}
       // in-line style using javascript {} using template string to call css rgb function rgb() and pass rgb values through const bgc as arg using interpolation
       style={{ backgroundColor: `rgb(${bgc})` }}
+      onClick={() => {
+        setAlert(true)
+        navigator.clipboard.writeText(hex)
+      }}
     >
-      <p className='percent-value'>
-        {weight}
-      </p>
+      <p className='percent-value'>{weight}%</p>
       <p className='color-value'>{hex}</p>
+      {/* conditional rendering */}
+      {alert && <p className='alert'>copied to clipboard</p>}
     </article>
   )
 }
