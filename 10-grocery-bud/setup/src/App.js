2923b46d64396c2672e58016eaff1644404056ee
Alex Velazquez <avelasquez075@gmail.com>
2021-05-17 12:30:05 -0700

    add state values

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,14 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  // id of item being edited
+  const [editID, setEditID] = useState(null)
+  // set up alert as object
+  const [alert, setAlert] = useState({show: false, msg: '', type: ''})
+
   return <h2>grocery bud setup</h2>
 }
-
 export default App
