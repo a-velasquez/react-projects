bb87d086009c232101bac0b8355986420286392e
Alex Velazquez <avelasquez075@gmail.com>
2021-05-21 21:00:45 -0700

    add useEffect with callback function to clear alert after 3 seconds

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -1,6 +1,12 @@
 import React, { useEffect } from 'react'
 
 const Alert = ({ msg, type, list, removeAlert }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert()
+    }, 3000)
+    return () => clearTimeout(timeout)
+  }, [])
   return (
     <p className={`alert alert-${type}`}>{msg}</p>
   )
