599f8728f9cc3fa203c726fc983450fecc5b42db
Alex Velazquez <avelasquez075@gmail.com>
2021-05-25 00:21:31 -0700

    add basic outline of navbar

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,21 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
+import { FaBars, FaCentercode, FaTwitter } from 'react-icons/fa'
 import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <div className='links-container show-container'>
+            <ul className='social-icons'>
+            </ul>  
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
