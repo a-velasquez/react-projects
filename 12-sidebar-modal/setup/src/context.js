53132636aca954619cbaf9cd0efe8bb979d17236
Alex Velazquez <avelasquez075@gmail.com>
2021-06-01 16:12:09 -0700

    add functions for opening and closing sidebar and modal

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -6,6 +6,22 @@ const AppProvider = ({children}) => {
 	const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 	const [isModalOpen, setIsModalOpen] = useState(false)
 
+	const openSidebar = () => {
+		setIsSidebarOpen(true)
+	}
+
+	const closeSidebar = () => {
+		setIsSidebarOpen(false)
+	}
+
+	const openModal = () => {
+		setIsModalOpen(true)
+	}
+
+	const closeModal = () => {
+		setIsModalOpen(false)
+	}
+
 	return <AppContext.Provider value={'hello'}>{children}</AppContext.Provider>
 }
 
