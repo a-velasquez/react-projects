b393cb734fd87a7297d1ed7e27b8b5cd64f3c427
Alex Velazquez <avelasquez075@gmail.com>
2021-06-02 12:56:52 -0700

    import and deconstruct useGlobalContext

--- 13-stripe-submenus/setup/src/Sidebar.js
+++ 13-stripe-submenus/setup/src/Sidebar.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { FaTimes } from 'react-icons/fa'
+import {FaTimes} from 'react-icons/fa'
 import sublinks from './data'
+import {useGlobalContext} from './context'
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+	const {isSidebarOpen, closeSidebar} = useGlobalContext()
+
+	return <h2>sidebar component</h2>
 }
 
 export default Sidebar
