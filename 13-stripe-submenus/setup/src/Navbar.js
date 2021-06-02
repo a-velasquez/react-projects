94a1cefa878b5d16811c99edfe9c66eee154c52f
Alex Velazquez <avelasquez075@gmail.com>
2021-06-01 23:53:18 -0700

    import useGlobalContext, pass functions, render nav and div

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
+import {FaBars} from 'react-icons/fa'
+import {useGlobalContext} from './context'
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
+	const {openSidebar, openSubmenu, closeSubmenu} = useGlobalContext()
+	return (
+		<nav className='nav'>
+			<div className='nav-center'>
+				<div className='nav-header'>
+					<img className='nav-logo' src={logo} alt='stripe' />
+				</div>
+				<ul className='nav-links'></ul>
+			</div>
+		</nav>
+	)
 }
 
 export default Navbar
