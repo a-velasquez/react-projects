8e2d7a7077de3b04d44877e9a4c874aa903d360b
Alex Velazquez <avelasquez075@gmail.com>
2021-06-02 23:15:05 -0700

    edit if bloc in useEffect for links.length > 3

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -22,7 +22,7 @@ const Submenu = () => {
 		if (links.length === 3) {
 			setColumns('col-3')
 		}
-		if (links.length === 4) {
+		if (links.length > 3) {
 			setColumns('col-4')
 		}
 	}, [location, links])
