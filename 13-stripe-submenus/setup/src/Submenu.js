8db31d698ff1556612ca48a71cb59c680cf465e7
Alex Velazquez <avelasquez075@gmail.com>
2021-06-02 22:30:55 -0700

    add location, useRef, useEffect to show submenu

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -2,10 +2,22 @@ import React, {useState, useRef, useEffect} from 'react'
 import {useGlobalContext} from './context'
 
 const Submenu = () => {
-	const {isSubmenuOpen} = useGlobalContext()
+	const {isSubmenuOpen, location} = useGlobalContext()
+	const container = useRef(null)
+
+	useEffect(() => {
+		const submenu = container.current
+		console.log(submenu)
+		const {center, bottom} = location
+		submenu.style.left = `${center}px`
+		submenu.style.top = `${bottom}px`
+	}, [location])
 
 	return (
-		<aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}>
+		<aside
+			className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}
+			ref={container}
+		>
 			<section>
 				<h4>section title</h4>
 				<div className='submenu-center'>links</div>
