271a129b4a17783602fba796fcd049b190ee3254
Alex Velazquez <avelasquez075@gmail.com>
2021-06-10 15:06:02 -0700

    add about with lorem text

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
+	return (
+		<section className='section about-section'>
+			<h1 className='section-title'>about us</h1>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae at,
+				fugiat eum repellat voluptates doloribus corporis. Iste rem laudantium,
+				asperiores, sapiente eaque ullam dicta officia rerum vitae perspiciatis
+				sit. Nulla.
+			</p>
+		</section>
+	)
 }
 
 export default About
