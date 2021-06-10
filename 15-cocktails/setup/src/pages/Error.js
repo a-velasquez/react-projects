c7b964a0aab200085a00cc6fef744a9ccde8cb38
Alex Velazquez <avelasquez075@gmail.com>
2021-06-10 15:08:34 -0700

    add section, div with text and link to home page

--- 15-cocktails/setup/src/pages/Error.js
+++ 15-cocktails/setup/src/pages/Error.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const Error = () => {
-  return (
-    <div>
-      <h2>error page</h2>
-    </div>
-  )
+	return (
+		<section className='error-page section'>
+			<div className='error-container'>
+				<h1>Oops, you've hit a dead end</h1>
+				<Link to='/' className='btn-primary'>
+					back to home page
+				</Link>
+			</div>
+		</section>
+	)
 }
 
 export default Error
