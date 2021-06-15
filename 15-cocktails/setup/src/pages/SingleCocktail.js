bab80950c29e99b33e6f030978d9520484384e46
Alex Velazquez <avelasquez075@gmail.com>
2021-06-15 12:05:10 -0700

    add state for cocktails, try/catch block for fetching cocktail

--- 15-cocktails/setup/src/pages/SingleCocktail.js
+++ 15-cocktails/setup/src/pages/SingleCocktail.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Loading from '../components/Loading'
-import { useParams, Link } from 'react-router-dom'
+import {useParams, Link} from 'react-router-dom'
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
 const SingleCocktail = () => {
-  return (
-    <div>
-      <h2>single cocktail page </h2>
-    </div>
-  )
+	const {id} = useParams()
+	const [loading, setLoading] = useState(false)
+	const [cocktail, setCocktail] = useState(null)
+
+	useEffect(() => {
+		setLoading(true)
+		const getCocktails = async () => {
+			try {
+				const response = await fetch(`${url}${id}`)
+				const data = await response.json()
+				console.log(data.drinks)
+				if (data.drinks) {
+				} else {
+					setCocktail(null)
+				}
+				setLoading(false)
+			} catch (error) {
+				console.log(error)
+				setLoading(false)
+			}
+		}
+		getCocktails()
+	}, [id])
+
+	return (
+		<div>
+			<h2>{id} </h2>
+		</div>
+	)
 }
 
 export default SingleCocktail
