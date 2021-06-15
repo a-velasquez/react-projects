51e30c6dd5261d9aeef7983f962a6fb1227f8e36
Alex Velazquez <avelasquez075@gmail.com>
2021-06-15 12:18:43 -0700

    grab first item in object for drinks, destrucutre object, return newCocktail

--- 15-cocktails/setup/src/pages/SingleCocktail.js
+++ 15-cocktails/setup/src/pages/SingleCocktail.js
@@ -14,8 +14,39 @@ const SingleCocktail = () => {
 			try {
 				const response = await fetch(`${url}${id}`)
 				const data = await response.json()
-				console.log(data.drinks)
 				if (data.drinks) {
+					const {
+						strDrink: name,
+						strDrinkThumb: image,
+						strAlcoholic: info,
+						strCategory: category,
+						strGlass: glass,
+						strInstructions: instructions,
+						strIngredient1,
+						strIngredient2,
+						strIngredient3,
+						strIngredient4,
+						strIngredient5
+					} = data.drinks[0]
+					const ingredients = {
+						strIngredient1,
+						strIngredient2,
+						strIngredient3,
+						strIngredient4,
+						strIngredient5
+					}
+					const newCocktail = {
+						name,
+						image,
+						info,
+						category,
+						glass,
+						instructions,
+						ingredients,
+						instructions
+					}
+					setCocktail(newCocktail)
+					console.log(newCocktail)
 				} else {
 					setCocktail(null)
 				}
