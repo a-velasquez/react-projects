35d8e86a6b6eeb3ea7ce9371c352031885da0ce6
Alex Velazquez <avelasquez075@gmail.com>
2021-06-15 11:37:42 -0700

    add searchValue w/useRef, add search form

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import {useGlobalContext} from '../context'
 
 const SearchForm = () => {
 	const {setSearchTerm} = useGlobalContext()
+	const searchValue = useRef('')
 	return (
-		<div>
-			<h2>search form component</h2>
-		</div>
+		<section className='section search'>
+			<form className='search-form'>
+				<div className='form-control'>
+					<label htmlFor='name'>search for your favorite cocktail</label>
+					<input type='text' id='name' ref={searchValue} />
+				</div>
+			</form>
+		</section>
 	)
 }
 
