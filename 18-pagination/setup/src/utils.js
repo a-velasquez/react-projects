c7c20a162e72f466115a421fe0da7dea19b5b96a
Alex Velazquez <avelasquez075@gmail.com>
2021-06-17 19:51:39 -0700

    change pages to numberOfPages to be verbose

--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,12 +1,12 @@
 const paginate = (followers) => {
 	const itemsPerPage = 10
-	const pages = Math.ceil(followers.length / itemsPerPage)
-	console.log(pages)
+	const numberOfPages = Math.ceil(followers.length / itemsPerPage)
 
-	const newFollowers = Array.from({length: pages}, (_, index) => {
+	const newFollowers = Array.from({length: numberOfPages}, (_, index) => {
 		const start = index * itemsPerPage
 		return followers.slice(start, start + itemsPerPage)
 	})
+
 	return newFollowers
 }
 
